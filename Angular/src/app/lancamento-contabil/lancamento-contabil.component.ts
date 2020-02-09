7aa7389d2ac26c124ee3dcc97a76a84dc1353bb5
ViniciusRio <viniciustrave@gmail.com>
2020-02-08 21:33:43 -0400

    Index de lançamentos
    Foi usado Moment.js para formatar a data do jeito que precisei

--- Angular/src/app/lancamento-contabil/lancamento-contabil.component.ts
+++ Angular/src/app/lancamento-contabil/lancamento-contabil.component.ts
@@ -1,4 +1,6 @@
 import { Component, OnInit } from '@angular/core';
+import { LancamentoContabilService } from '../services/lancamento-contabil/lancamento-contabil.service';
+import * as moment from 'moment';
 
 @Component({
   selector: 'app-lancamento-contabil',
@@ -6,10 +8,25 @@ import { Component, OnInit } from '@angular/core';
   styleUrls: ['./lancamento-contabil.component.css']
 })
 export class LancamentoContabilComponent implements OnInit {
+  lancamentos;
+  now = moment().format('dddd');
 
-  constructor() { }
+
+  constructor(private lancamentoService: LancamentoContabilService) { }
 
   ngOnInit() {
+    this.listarLancamentos();
   }
 
+  converterData(dataAntiga: string)
+  {
+    return moment(dataAntiga, "YYYY/MM/DD").format("DD/MM/YYYY");
+  }
+  
+  listarLancamentos() {
+    this.lancamentoService.todosLancamentos().then(resultado => {
+      this.lancamentos = resultado;
+      console.log("LANÇAMENTOS: ", this.lancamentos);
+    });
+  }
 }
